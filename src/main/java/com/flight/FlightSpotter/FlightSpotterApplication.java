06f5cd2d93a45889a8527ed0a1e1008f193459b9
Nemanja Djuric <nemanja.djuric@outlook.com>
2020-05-10 19:52:15 +0200

    Initial Spring commit

---
+++ src/main/java/com/flight/FlightSpotter/FlightSpotterApplication.java
@@ -0,0 +1,13 @@
+package com.flight.FlightSpotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSpotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flight/FlightSpotter/FlightSpotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.FlightSpotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSpotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
